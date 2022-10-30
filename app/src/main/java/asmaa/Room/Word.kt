d50da384cd068905f023605c2e60af1c82ab668b
Asmaa-Khaled21 <93935870+Asmaa-Khaled21@users.noreply.github.com>
2022-10-30 06:35:43 +0200

    finish

--- app/src/main/java/asmaa/Room/Word.kt
+++ app/src/main/java/asmaa/Room/Word.kt
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey
 
 @Entity (tableName = "word_table")
 class Word (
-    @PrimaryKey (autoGenerate = true)
-    val id : Int,
+    @PrimaryKey //(autoGenerate = true)
+    //val id : Int,
     @ColumnInfo (name = "word")
     val word : String )
